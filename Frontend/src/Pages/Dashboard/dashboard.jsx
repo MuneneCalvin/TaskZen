4025c777ae953e689b612861553dc09fdd26d7a8
MuneneCalvin <munenecalvn@gmail.com>
2023-07-11 01:09:14 +0300

    Added side nav

--- Frontend/src/Pages/Dashboard/dashboard.jsx
+++ Frontend/src/Pages/Dashboard/dashboard.jsx
@@ -1,123 +1,50 @@
-import './dashboard.css'
+import { Link } from 'react-router-dom'
+// import userimg from '../../assets/images/user.png';
+import dashboard from '../../assets/images/dashboard.png';
+import reports from '../../assets/images/reports.png';
+import messages from '../../assets/images/messages.png';
+import projects from '../../assets/images/projects.png';
+import members from '../../assets/images/members.png';
+import setting from '../../assets/images/setting.png';
+import logo from '../../assets/logo.png';
+import logout from '../../assets/images/logout.png';
 
-function dashboard() {
+function dashboard2() {
   return (
     <>
-    <div className="sidebar">
-      <div className="sidebar-header">
-        <h3 className="brand">
-          <span className="ti-unlink"></span>
-          <span>Dashboard</span>
-        </h3>
-        <span className="t-menu-alt"></span>
-      </div>
-      <div className="sidebar-menu">
-        <ul>
-          <li>
-            <a href="#">
-              <span className="ti-home"></span>
-              <span>Home</span>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <span className="ti-face-smile"></span>
-              <span>Team</span>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <span className="ti-calendar"></span>
-              <span>Calendar</span>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <span className="ti-agenda"></span>
-              <span>Tasks</span>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <span className="ti-folder"></span>
-              <span>Projects</span>
-            </a>
-          </li>
-          <li>
-            <li>
-            <a href="#">
-              <span className="ti-book"></span>
-              <span>Contacts</span>
-            </a>
-          </li>
-            <a href="#">
-              <span className="ti-settings"></span>
-              <span>Settings</span>
-            </a>
-          </li>
-          </ul>
-      </div>
-    </div>
-
-    <div className="main-content">
-      <header>
-        <div className='search-wrapper'>
-          <span className="ti-search"></span>
-          <input type="search" placeholder="Search" />
-        </div>
-        <div className="social-icons">
-          <span className="ti-bell"></span>
-          <span className="ti-comment"></span>
-          <div></div>
-        </div>
-      </header>
-
-      <main>
-        <h2 className="dash-title">Overview</h2>
-        <div className="dash-cards">
-          <div className="card-single">
-            <div className="card-body">
-              <span className="ti-briefcase"></span>
-              <div>
-                <h5>Account Balance</h5>
-                <h4>$30,659.45</h4>
-              </div>
+      <div id='Header'>
+            <div className="side-nav">
+                <div className="user">
+                    <img src={logo} className='user-img' />
+                    <div className='user-details'>
+                        <h2>user.username</h2>
+                        <p>user.email</p>
+                    </div>
+                </div>
+                <ul>
+                    <li><img src={dashboard} /><Link  className='side-link'>Book Now</Link></li>
+                    <li><img src={reports} /><Link  className='side-link'>My Bookings</Link></li>
+                    <li><img src={messages} /><Link to="/Contact" className='side-link'>Contact Us</Link></li>
+                    <li><img src={projects} /><Link to="/About" className='side-link'>About Us</Link></li>
+                    <li><img src={members} /><Link  className='side-link'>My Profile</Link></li>
+                    <li><img src={setting} /><Link to="/Setting" className='side-link'>Settings</Link></li>
+                </ul>
+                <ul>
+                    <li><img src={logout} /><Link className='log-out'>Log Out</Link></li>
+                </ul>
             </div>
-            <div className="card-footer">
-              <a href="#">View all</a>
-              </div>
-          </div>
 
-          <div className="card-single">
-            <div className="card-body">
-              <span className="ti-reload"></span>
-              <div>
-                <h5>Pending</h5>
-                <h4>$19,500.45</h4>
-              </div>
+            <div className="top-nav">
+                <div className="menu">
+                    <h3>Dashboard</h3>
+                    <div className="search">
+                        <input type="text" placeholder="Search" />
+                        <i className="fas fa-search"></i>
+                        
             </div>
-            <div className="card-footer">
-              <a href="#">View all</a>
-              </div>
           </div>
-
-          <div className="card-single">
-            <div className="card-body">
-              <span className="ti-checkbox"></span>
-              <div>
-                <h5>Processed</h5>
-                <h4>$20,659.45</h4>
-              </div>
-            </div>
-            <div className="card-footer">
-              <a href="#">View all</a>
-              </div>
-          </div>
-        </div>
-      </main>
-    </div>
     </>
   )
 }
 
-export default dashboard
+export default dashboard2
