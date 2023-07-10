048bae1b7500af6d9cda9b297f3f8cd4160663a3
MuneneCalvin <munenecalvn@gmail.com>
2023-07-10 11:49:00 +0300

    Added pages

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,22 +1,22 @@
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Landing from './Pages/Landing';
+import Register from './Pages/signup';
+import Login from './Pages/Login';
+import NotFound from './Pages/NotFound';
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/signup" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
