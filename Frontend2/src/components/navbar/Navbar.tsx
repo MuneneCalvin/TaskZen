044f316512e02ec8dc87359f0069f2d8acfe05b6
MuneneCalvin <munenecalvn@gmail.com>
2023-07-11 22:10:58 +0300

    Updated navbar

--- Frontend2/src/components/navbar/Navbar.tsx
+++ Frontend2/src/components/navbar/Navbar.tsx
@@ -5,24 +5,21 @@ const Navbar = () => {
     <div className="navbar">
       <div className="logo">
         <img src="logo.svg" alt="" />
-        <span>lamadmin</span>
+        <span>TaskZen</span>
       </div>
       <div className="icons">
-        <img src="/search.svg" alt="" className="icon" />
-        <img src="/app.svg" alt="" className="icon" />
-        <img src="/expand.svg" alt="" className="icon" />
+        <img src="/search.svg" alt="" className="icon-nav" />
+        <img src="/app.svg" alt="" className="icon-nav" />
+        <img src="/expand.svg" alt="" className="icon-nav" />
         <div className="notification">
           <img src="/notifications.svg" alt="" />
           <span>1</span>
         </div>
+        <img src="/settings.svg" alt="" className="icon-nav" />
         <div className="user">
-          <img
-            src="https://images.pexels.com/photos/11038549/pexels-photo-11038549.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
-            alt=""
-          />
           <span>Jane</span>
+          <img src="https://images.pexels.com/photos/11038549/pexels-photo-11038549.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt="" />
         </div>
-        <img src="/settings.svg" alt="" className="icon" />
       </div>
     </div>
   );
