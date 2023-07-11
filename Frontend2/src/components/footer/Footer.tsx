792cec36065797674fb40639c45ea331fefbaf7f
MuneneCalvin <munenecalvn@gmail.com>
2023-07-11 22:01:11 +0300

    Updated footer

--- Frontend2/src/components/footer/Footer.tsx
+++ Frontend2/src/components/footer/Footer.tsx
@@ -3,8 +3,8 @@ import "./footer.scss"
 const Footer = () => {
   return (
     <div className="footer">
-      <span>lamadmin</span>
-      <span>© Lama Dev Admin Dashboard</span>
+      <span>Xe7eN</span>
+      <span>© Shawn</span>
     </div>
   )
 }
