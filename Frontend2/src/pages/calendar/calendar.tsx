830f5b974f8cb333b0a0ef5fd425c3a66020d293
MuneneCalvin <munenecalvn@gmail.com>
2023-07-12 16:47:34 +0300

    implemented calendar scheduler

--- Frontend2/src/pages/calendar/calendar.tsx
+++ Frontend2/src/pages/calendar/calendar.tsx
@@ -1,8 +1,14 @@
-
+import { Inject, ScheduleComponent, Day, Week, WorkWeek, Month, Agenda } from '@syncfusion/ej2-react-schedule'
+import './calendar.scss'
 
 function calendar() {
     return (
-        <div>calendar</div>
+        <div className='Calendar'>
+            <ScheduleComponent style={{height: "100vh"}} currentView='Month' >
+                <Inject services={[Day, Week, WorkWeek, Month, Agenda]} ></Inject>
+            </ScheduleComponent>
+            
+        </div>
     )
 }
 
