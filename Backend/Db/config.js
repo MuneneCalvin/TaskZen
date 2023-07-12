7601b79b2e0b8c81c631d7d19effb63ba4325d5f
MuneneCalvin <munenecalvn@gmail.com>
2023-07-12 12:13:04 +0300

    Fixed configurations

--- Backend/Db/config.js
+++ Backend/Db/config.js
@@ -3,16 +3,14 @@ import assert from 'assert';
 
 dotenv.config();
 
-const { PORT, HOST, SQL_USER, SQL_PASSWORD, SQL_DATABASE, SQL_SERVER, JWT_SECRET } = process.env;
+const { PORT, SQL_USER, SQL_PASSWORD, SQL_DATABASE, SQL_SERVER, JWT_SECRET } = process.env;
 
 const sqlEncrypt = process.env.SQL_ENCRYPT === "true";
 
 assert(PORT, 'PORT is required');
-assert(HOST, 'HOST is required');
 
 const config = {
     port: PORT,
-    host: HOST,
     sql: {
         server: SQL_SERVER,
         database: SQL_DATABASE,
