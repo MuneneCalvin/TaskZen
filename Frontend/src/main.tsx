eb6631cfc15dcd00a7e283f09fa2a4a8b7791942
MuneneCalvin <munenecalvn@gmail.com>
2023-07-18 12:26:44 +0300

    Added login middleware

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import ReactDOM from 'react-dom/client';
+import { ContextProvider } from './context/userContext/Context.jsx';
+import App from './App.tsx';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ContextProvider>
+      <App />
+    </ContextProvider>
   </React.StrictMode>,
 )
