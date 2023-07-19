d2e9d6eb77c73b142f9c2eefe7957a6d20505332
MuneneCalvin <munenecalvn@gmail.com>
2023-07-19 11:15:10 +0300

    Updated navbar

--- Frontend/src/components/navbar/Navbar.tsx
+++ Frontend/src/components/navbar/Navbar.tsx
@@ -8,14 +8,14 @@ const Navbar = () => {
         <span>TaskZen</span>
       </div>
       <div className="icons">
-        <img src="/search.svg" alt="" className="icon-nav" />
-        <img src="/app.svg" alt="" className="icon-nav" />
+        {/* <img src="/search.svg" alt="" className="icon-nav" /> */}
+        {/* <img src="/app.svg" alt="" className="icon-nav" /> */}
         <img src="/expand.svg" alt="" className="icon-nav" />
         <div className="notification">
           <img src="/notifications.svg" alt="" />
           <span>1</span>
         </div>
-        <img src="/settings.svg" alt="" className="icon-nav" />
+        {/* <img src="/settings.svg" alt="" className="icon-nav" /> */}
         <div className="user">
           <span>Shawn</span>
           <img src="https://images.pexels.com/photos/11038549/pexels-photo-11038549.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt="" />
