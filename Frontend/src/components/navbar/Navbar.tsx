590df00fbcb8678e610f9f81740ad6785da53c54
MuneneCalvin <munenecalvn@gmail.com>
2023-07-25 20:02:58 +0300

    Update navbar

--- Frontend/src/components/navbar/Navbar.tsx
+++ Frontend/src/components/navbar/Navbar.tsx
@@ -1,12 +1,11 @@
 import { useState, useContext } from "react";
 import { Context } from "../../context/userContext/Context";
-import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "./navbar.scss";
 
 const Navbar = () => {
   const { user, dispatch } = useContext(Context);
   const navigate = useNavigate();
-  // const { id } = useParams();
   const username = user?.username ?? "Guest";
   const [showDropdown, setShowDropdown] = useState(false);
   const [notificationCount, setNotificationCount] = useState(0);
